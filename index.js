342806e949a47cf730ed0ec141f6f26dc961b754
minhaz-bin-monib <minhazbm463448@gmail.com>
2021-03-30 16:16:01 +0600

    root problem solved

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.use(cors())
 
 const port = 5000
 
+app.get("/",(req,res)=>{
+  res.send("hello it's db form working ")
+})
+
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
